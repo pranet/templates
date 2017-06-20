de1379a370928dfa341a4ae3faf621fc1ea3cdbd
Pranet Verma <pranetverma@gmail.com>
2017-06-21 02:51:21 +0530

    Plain lombok logging using sl4j

---
+++ lomboklogging/src/main/java/com/beyondcp/lomboklogging/Main.java
@@ -0,0 +1,15 @@
+package com.beyondcp.lomboklogging;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Created by pranet on 21/06/17.
+ */
+@Slf4j
+public class Main {
+
+    public static void main(String[] args) {
+        log.debug("lol");
+    }
+
+}
